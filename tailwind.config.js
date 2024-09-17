35b8cdef2d255be9a729f4a8600b0cb861eee610
Rico Eriansyah <102507449+ricoerian@users.noreply.github.com>
2024-09-17 14:27:33 +0000

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#17171F',
+        'selected-text': '#F62BA5',
+        'theme': '#3F3FFF',
+        'nav': '#404053',
+        'secondary': '#9191a4',
+        'badge': '#3F3F51',
+        'input-border': '#565666',
+        'input': '#2A2A35'
+      },
+      'fontFamily': {
+        'poppins': ['Poppins', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
+
