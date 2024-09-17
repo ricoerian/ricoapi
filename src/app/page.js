240270da17eb33354c8a4fd37947aa13be6dd916
Rico Eriansyah <102507449+ricoerian@users.noreply.github.com>
2024-09-17 17:24:30 +0000

    speedinsights

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 export default function Home() {
@@ -265,6 +266,7 @@ export default function Home() {
   
             <script src="/assets/js/index.js"></script>
             <Analytics />
+            <SpeedInsights />
         </body>
         </html>
     );
