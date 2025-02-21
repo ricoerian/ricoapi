52a961fa7df346e1b312e45ce8ae86c526edc154
Rico Eriansyah <richoeriansyahm@gmail.com>
2025-02-21 17:56:33 +0700

    fixing resume

--- src/components/Organisms/Hero.jsx
+++ src/components/Organisms/Hero.jsx
@@ -27,7 +27,7 @@ export const Hero = () => {
                             Repositories
                         </button>
                     </a>
-                    <a href="https://www.linkedin.com/ambry/?x-li-ambry-ep=AQJCNEO9tGSXzwAAAZUn5BTOvORherzSD8qlAIkX-1vq7ufacbXpNnLL8Bp0FoVS9SSEP55HVs6oSD3tyOGeuM5GU6tC1YGPuX8mXf-XxxX-Z-MTJ9n8lZUW7Iq72oVf5dX7Gl65TJ5Hf2LmU3fCsVOPrxBh3IPrMw138C3TrK9VDQLXyRzaeNiue6_nLRY8QptxZ0VrWg3XjzblQxdGL-VlDB9sYPmY5sRuUChBvvOe1H5g1jdacnfZhYruauZX57q6vtToB2CY47jMFyxG_IykQW1Ic-zsq5ubp75z2_HvVcOshucd_5j0wCwNGogC3LHDxQuNDM-EX7ILq5QcbsLbpjTVaCYD14VOV_qvQetaAiIzHWj8sNDn3Nd2BuhxaVWHtcHf8qNzT3AiL3E1IzkarkXymD_KqyyI4euarDaUlZXQy4F3KqmceM2Kdsa2JrOvc6aVsrzPTUdl3CQ-0rF068kzUFFVl9W0VUulN1NXziDuwJXLwZJ-970DDLLiq3_g88LY76D3L2qpxNghi6LcrcGnjjxrLzwyUQtQJYtNiVoPDM-PXTZYR7aCG_KiQkJ-GQ&x-ambry-um-filename=Resume-Rico-Eriansyah.pdf" target="_blank" rel="noopener noreferrer">
+                    <a href="https://drive.google.com/file/d/1aYBc-TJPMAYrI2TOgITIn_LqfyP4kvyx/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                         <button className="px-8 py-4 bg-button rounded-xl text-white font-bold font-poppins my-8 flex items-center text-xl hover:border-2 hover:bg-slate-800">
                             Resume
                         </button>
